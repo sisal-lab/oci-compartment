af341da41ff41d77fab8de9dda24cf3289b6a6e8
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2022-06-24 19:19:11 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,3 +15,16 @@ provider "oci" {
   private_key = var.private_key
   fingerprint = var.fingerprint
 }
+
+
+resource "oci_identity_compartment" "main" {
+  #Required
+  compartment_id = var.tenancy_ocid
+  
+  name = "Sisal-Engineering"
+  description = "Compartment dedicated to Engineering activities"
+  
+  #Optional
+  #defined_tags = {"Operations.CostCenter"= "42"}
+  #freeform_tags = {"Department"= "Finance"}
+}
